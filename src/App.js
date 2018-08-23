a98c544fdd31e76f67c18f882d425a35c5a02848
jcgallardomorales@gmail.com <jcgallardomorales@gmail.com>
2018-08-23 21:00:14 +0200

    material-ui, eslint and ciclelife of classComponent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import WeatherLocation from  './components/WeatherLocation';
 //import logo from './logo.svg';
 import './App.css';
@@ -6,9 +7,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <WeatherLocation></WeatherLocation>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <WeatherLocation></WeatherLocation>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,22 +1,10 @@
 import React, {Component} from 'react';
+//import { Preloader } from 'react-materialize';
+import CircularProgress from 'material-ui/CircularProgress';
 import Location from './Location';
 import WeatherData  from './WeatherData';
-import { SUN } from '../../constants/weathers';
+import transformWeather from './../../services/transformWeather';
 import './styles.css';
-
-const data1 = {
-    temperature: 20,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s',
-};
-
-/*const data2 = {
-    temperature: 12,
-    weatherState: WINDY,
-    humidity: 20,
-    wind: '25 m/s',
-};*/
 
 const KEY = 'bfff2827e97e0b54a2d60cf2db8d2209';
 const LOCATION = 'Madrid,es';
@@ -27,8 +15,34 @@ class WeatherLocation extends Component {
         super();
         this.state = {
             city: "Madrid",
-            data: data1
+            data: null
         };
+    }
+
+    componentWillMount = () => { // execute once
+        //console.log("1) componentWillMount")
+        this.handleUpdateClick();
+    }
+
+    componentDidMount = () => { // execute once
+        //console.log("3) componentDidMount");
+    }
+    componentWillUpdate = () => { // execute few times
+        //console.log("4) componentWillUpdate");
+    }
+    componentDidUpdate = () => { // execute few times
+        //console.log("6) componentDidUpdate");
+    }
+    
+    render = () => { // execute few times
+        //console.log("2), 5)render");
+        const { city, data } = this.state;
+        return (
+            <div className="weather-location-cont">
+                <Location city={city} />
+                {data ? <WeatherData data={data} /> : <CircularProgress  />}
+            </div>
+        );
     }
 
     handleUpdateClick = () => {
@@ -37,40 +51,11 @@ class WeatherLocation extends Component {
         fetch(api_weather)
             .then(res => res.json())
             .then(weatherData => {
-                const data = this.getData(weatherData);
+                const data = transformWeather(weatherData);
                 this.setState({
                     data: data // is not neccesary include param city because new value is equal to original value
                 });
             });
-    }
-    getWeatherState = weather => {
-        return SUN;
-    }
-
-    getData = (weather_data) => {
-        const {humidity, temp} = weather_data.main;
-        const {speed} = weather_data.wind;
-        const weatherState = this.getWeatherState(this.weather_data);
-
-        const data =  {
-            humidity, // ecmascript simplifica "humidity : humidity"
-            temperature: Math.round(temp - 273.15),
-            weatherState,
-            wind: `${speed} m/s`,
-        };
-        
-        return data;
-    }
-
-    render = () => {
-        const { city, data } = this.state;
-        return (
-            <div className="weather-location-cont">
-                <Location city={city} />
-                <WeatherData data={data} />
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
-            </div>
-        );
     }
 }
 
