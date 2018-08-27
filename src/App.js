ee90ac099b5e1fe69cad1472e603941093680a56
jcgallardomorales@gmail.com <jcgallardomorales@gmail.com>
2018-08-27 20:04:30 +0200

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LocationList from  './components/LocationList';
+import { Grid, Row, Col} from 'react-flexbox-grid';
+//import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -17,18 +20,43 @@ class App extends Component {
     console.log(`handleSelectionLocation ${city}`);
   }
 
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div className="App">
-          <LocationList 
-            cities={cities} 
-            onSelectedLocation={this.handleSelectedLocation}
-          />
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+    render() {
+        /*return (
+            <MuiThemeProvider>
+                <Grid fluid>
+                    <Row>
+                        <Col xs={12} sm={6} md={4}>
+                            <div className="red"></div>
+                        </Col>
+                    </Row>
+                </Grid>
+            </MuiThemeProvider>
+        )
+        */
+
+        return (
+            <MuiThemeProvider>
+                <Grid>
+                    <Row>
+                        <Col xs={12}>
+                            <AppBar title="Weather App"></AppBar>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={6}>
+                            <LocationList 
+                                cities={cities} 
+                                onSelectedLocation={this.handleSelectedLocation}
+                            />
+                        </Col>
+                        <Col cs={12} md={6}>
+                            <div className="detail"></div>
+                        </Col>
+                    </Row>
+                </Grid>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
